f25bde66ac4b5cfbfe87b7741cc91b0642376912
oojas <oojasgupta@gmail.com>
2022-01-05 23:55:54 +0530

    UI login complete

--- lib/Authentication/login.dart
+++ lib/Authentication/login.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -13,29 +14,77 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // ignore: sized_box_for_whitespace
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: Column(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height / 1.6,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  color: Colors.amberAccent,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(13),
-                    topRight: Radius.circular(12),
-                    bottomLeft: Radius.circular(1000),
-                  )),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                // ignore: prefer_const_literals_to_create_immutables
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
                 children: [
-                  Text('Ojas')],
+                  Container(
+                    child: Center(
+                        child: Padding(
+                      padding: const EdgeInsets.only(bottom: 200.0),
+                      child: Text(
+                        "Sign Up",
+                        style: GoogleFonts.heebo(
+                            fontSize: 30,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )),
+                    height: MediaQuery.of(context).size.height / 1.5,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.amberAccent,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(13),
+                          topRight: Radius.circular(12),
+                          bottomLeft: Radius.circular(700),
+                        ),
+                        gradient: LinearGradient(
+                            colors: [
+                              Colors.blue.shade300
+                                  .withAlpha(30)
+                                  .withGreen(50)
+                                  .withOpacity(0.6),
+                              Colors.pink.shade200
+                                  .withGreen(30)
+                                  .withRed(10)
+                                  .withOpacity(0.7),
+                              Colors.indigo.shade300
+                                  .withGreen(30)
+                                  .withOpacity(0.9)
+                            ],
+                            begin: Alignment.bottomRight,
+                            end: Alignment.topRight)),
+                  ),
+                  Positioned(
+                    bottom: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25)),
+                        elevation: 15,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width / 1.1,
+                          height: 300,
+                          decoration: BoxDecoration(),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
+              Image.asset(
+                'Assets/images/decoration.jpg',
+                height: 280,
+              )
+            ],
+          ),
         ),
       ),
     );
